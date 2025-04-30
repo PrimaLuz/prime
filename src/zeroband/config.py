54245d58b746abd071ddaf708fb6b6788b5a278e
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-04-30 00:35:17 +0000

    add diloco delayed default vlaue

--- src/zeroband/config.py
+++ src/zeroband/config.py
@@ -108,7 +108,7 @@ class DilocoConfig(BaseConfig):
     outer_lr: float = 0.7
     inner_steps: int
     compression: Compression = Compression.NO
-    delayed_update: bool
+    delayed_update: bool = False
 
 
 class MemoryProfilerConfig(BaseConfig):
